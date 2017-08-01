477d0dcad2a8d26fc37a2f22eb09f5efc1ff64db
GD Ziabletsev Sergei <ziabletsev-s@glode.co.jp>
2017-08-01 15:00:12 +0900

    test

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('push', function(event) {
 
   const title = 'グローバルデザイン';
   const options = {
-    body: "${event.data.text()}",
+    body: ${event.data.text()},
     icon: 'img/logo_m.gif',
     badge: 'img/logo_m.gif'
   };
